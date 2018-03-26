c8f88919f670d4a5f43a53662e0d9dff2432e89f
szelazny <scott_z_1@yahoo.com>
2018-03-26 14:36:21 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -72,7 +72,8 @@ var Location = function(title, lng, lat, venueId, cat) {
 		// Assigns a click event listener to the marker to open the info window.
 		this.addListener = google.maps.event.addListener(self.marker,'click', (this.openInfowindow));
 	        // Assigns a mouseover event listener to the marker t
-	       this.addListener = google.maps.event.addListener(self.marker,'mouseover', function() {alert('Made it to mouseover!');});
+	       //this.addListener = google.maps.event.addListener(self.marker,'mouseover', function() {alert('Made it to mouseover!');});
+                 this.addListener = google.maps.event.addListener(self.marker,'mouseover', function() {this.setAnimation(google.maps.Animation.BOUNCE);});
 
 
 		//this.addListener = google.maps.event.addListener(self.marker,'mouseover', function() {alert('Made it to mouseover!');});
